795e57630c1570e6ccc37382f924db65eda6770e
Eduardo <luiz.dudu3@gmail.com>
2022-06-30 10:03:23 -0300

    estrutura basica do projeto e about

---
+++ utils/utils.js
@@ -0,0 +1,20 @@
+function getAboutTeam(){
+    return (
+        [
+            {
+                name: "Luiz Eduardo",
+                photo: "https://i.imgur.com/pLgxs0G.jpg",
+                matricula: "509510",
+                email: "luizedugx@alu.ufc.br"
+            }, 
+            {
+                name: "Victor Mota",
+                photo: "https://i.imgur.com/l02Y5pC.jpg",
+                matricula: "509223",
+                email: "victor2018mota@gmail.com"
+            }
+        ]
+    )
+}
+
+module.exports.getAboutTeam = getAboutTeam
